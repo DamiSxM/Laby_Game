5dd63b4130622c65e177c1fb8745ea6fdb1b45e4
Dami <dami.bachelet@gmail.com>
2017-07-07 12:05:58 +0200

    Initial commit

---
+++ Laby_Game/Game.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Laby_Game
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "Game";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Game_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Laby_Game/Game.cs
@@ -0,0 +1,80 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+using Laby_Interfaces;
+using Laby_Maze;
+using Laby_Affichage;
+using Laby_Reseau;
+using Laby_Gestion;
+
+namespace Laby_Game
+{
+    public partial class Game : Form
+    {
+        Maze Labyrinthe;
+        LabyPanel Affichage;
+        Reseau Liaison;
+        Gestion Gestion;
+        public Game()
+        {
+            InitializeComponent();
+
+            Labyrinthe = new Maze(51);
+
+            Affichage = new LabyPanel(Labyrinthe.Labyrinthe);
+            Affichage.Location = new Point(0, 0);
+            Controls.Add(Affichage);
+
+            Liaison = new Reseau();
+
+            Gestion = new Gestion(Labyrinthe.Labyrinthe, Affichage, Liaison);
+        }
+
+        private void Game_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    Affichage.PersoMove(Direction.LEFT); break;
+                case Keys.Up:
+                    Affichage.PersoMove(Direction.UP); break;
+                case Keys.Right:
+                    Affichage.PersoMove(Direction.RIGHT); break;
+                case Keys.Down:
+                    Affichage.PersoMove(Direction.DOWN); break;
+                case Keys.A:
+                    Affichage.ItemAdd(3, 3, "porte1"); break;
+                case Keys.Z:
+                    Affichage.ItemRemove(3, 3); break;
+                case Keys.E:
+                    Affichage.ItemAdd(4, 4, "GrilleFermee"); break;
+                case Keys.Q:
+                    Affichage.PlayerAdd("bob", 5, 5); break;
+                case Keys.S:
+                    Affichage.PlayerMove("bob", 5, 4); break;
+                case Keys.D:
+                    Affichage.PlayerRemove("bob"); break;
+                case Keys.NumPad0:
+                    Affichage.Warfog(0); break;
+                case Keys.NumPad1:
+                    Affichage.Warfog(1); break;
+                case Keys.NumPad2:
+                    Affichage.Warfog(2); break;
+                case Keys.NumPad3:
+                    Affichage.Warfog(3); break;
+                case Keys.NumPad4:
+                    Affichage.Warfog(4); break;
+                case Keys.D1:
+                    Affichage.PersoTeleport(0, 0); break;
+                case Keys.D2:
+                    Affichage.PersoTeleport(1, 1); break;
+                case Keys.D3:
+                    Affichage.PersoTeleport(3, 3); break;
+            }
+        }
+
+        private void Game_KeyUp(object sender, KeyEventArgs e)
+        {
+            Affichage.PersoStop();
+        }
+    }
+}
